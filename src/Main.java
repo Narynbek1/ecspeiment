37ebc5d6431ae52e895fc977f1abb787b55de025
naryn <narynbeknurbekov@gmail.com>
2023-03-25 21:26:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        vararg(new int []{18,19,20},"Bectur", "timur","Bek");
+//    }
+//    static void vararg(int[] ages, String...name) {
+//        for (int i = 0; i < ages.length; i++) {
+//            System.out.println(name[i] +"  "+ ages[i] + "  лет");
+//        }
+//        String a = "  I like java!!!";
+//        System.out.println(a.charAt(a.length()-1));
+//        System.out.println(a.startsWith("I like"));
+//        System.out.println(a.endsWith("I like"));
+//        System.out.println(a.endsWith("!!!"));
+//        System.out.println(a.replace("a","o"));
+//        System.out.println(a.toUpperCase());
+//        System.out.println(a.toLowerCase());
+//        System.out.println(a.length());
+//        System.out.println(a);
+//        System.out.println(a.trim());
+//
+//
+//    }
+//    static void method(){
+//        String[] s = new String[]{"  I like java!!!"};
+//        for (String value : s) {
+//            System.out.println("  java");
+//
+//        }
+    }
+
+}
